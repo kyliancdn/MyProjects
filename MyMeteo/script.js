211ab5fc77d3843b16557bcb1f1f54b1bfe95b8c
PRFE 17 <prfe17@assofac.org>
2023-04-05 17:04:19 +0200

    second commit

--- MyMeteo/script.js
+++ MyMeteo/script.js
@@ -3,39 +3,57 @@
 // }
 
 // ipFind
-
+const iconReference = {
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-sunny",
+    "Snow": "wi wi-day-snow",
+    "mist": "wi wi-day-fog",
+    "Drizzle": "wi wi-day-sleet",
+}
 async function main() {
 
     const villeNm = document.querySelector('#ville')
     villeNm.contentEditable = true
-    console.log(villeNm.value);
+    console.log(villeNm.textContent);
 
-    const temp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${villeNm.textContent}&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric`)
+    const temperature = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${villeNm.textContent}&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric`)
         .then(res => res.json())
-        .then(data => { Math.round(data.main.temp)
-        //console.log(Math.round(data.main.temp))
-             })
+        .then(data => Math.round(data.main.temp))
+    
             
-    const desc = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric')
+    const desc = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${villeNm.textContent}&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric`)
     .then(res => res.json())
-    .then(data => data.weather[0].description)
+    .then(data => 
+        data.weather[0].description)
 
-    const ville = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Londres&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric')
+    const cond = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${villeNm.textContent}&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric`)
+    .then(res => res.json())
+    .then(data => data.weather[0].main)
+
+    
+
+    const ville = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${villeNm.textContent}&appid=cd73f70f866d3ff56bb0c172f4df20b1&lang=fr&units=metric`)
         .then(res => res.json())
         .then(data => data.name)
 
-    console.log(temp, desc, ville)
+    console.log(temperature, desc, ville)
     
     document.querySelector('#ville').textContent = ville
-    document.querySelector('#temperature').textContent = temp
+    document.querySelector('#temperature').textContent = temperature
     document.querySelector('#conditions').textContent = desc
+
+    document.querySelector('i.wi').className = iconReference[cond]
     
     villeNm.addEventListener('keydown', (e) => {
         if(e.keyCode === 13){
             e.preventDefault()
+            main()
         }
     })
 }
 
 main()
+
+
 
